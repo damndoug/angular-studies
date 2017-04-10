597839215e8e3a4e053dba200bd9039670078416
Douglas Pires <douglas.pires@live.com>
2017-04-09 22:10:53 -0300

    okw

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  private _cuisines: FirebaseListObservable<any[]>;
+  private _restaurants: Observable<any[]>;
+
+  constructor(private _af: AngularFire) {}
+
+  ngOnInit() {
+    
+    this._cuisines = this._af.database.list('/cuisines')
+    this._restaurants = this._af.database.list('/restaurants', {
+
+      query: {
+        orderByChild: 'rating',
+        equalTo: 5
+      }
+    }).map(restaurants => {
+
+        restaurants.map(restaurant => {
+
+          restaurant.cuisineType = this._af.database.object('/cuisines/' + restaurant.cuisine);
+          restaurant.featureType = [];
+
+
+
+          for (var feature in restaurant.features) {
+
+            restaurant.featureType.push(
+              this._af.database.object('/features/' + feature)
+            )
+          }
+        })
+
+        return restaurants
+
+      })
+  }
+
+  add(){
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {  } from "module";
+import { AngularFireModule } from "angularfire2";
 
 import { AppComponent } from './app.component';
+
+export const fireBaseConfig = {
+    apiKey: "AIzaSyDP5HKphu0W2JOCGgABa1K2BOi8LmEISGU",
+    authDomain: "pepper-6d2d2.firebaseapp.com",
+    databaseURL: "https://pepper-6d2d2.firebaseio.com",
+    projectId: "pepper-6d2d2",
+    storageBucket: "pepper-6d2d2.appspot.com",
+    messagingSenderId: "123813649818"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +23,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(fireBaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
